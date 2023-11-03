1b1d94be171409d25a24ff58daf4898fbaaff9ef
Michael Bertram <141644516+bertbigbite@users.noreply.github.com>
2023-11-03 10:28:39 +0000

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UserResults from "../components/Users/UserResults";
+import UserSearch from "../components/Users/UserSearch";
+function Home() {
+  return (
+    <div>
+      <UserSearch />
+      <UserResults />
+    </div>
+  );
+}
+export default Home;
